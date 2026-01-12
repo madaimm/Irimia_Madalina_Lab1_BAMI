6d8ad8587c3e1cd74dea66c4401aa69d62ab2838
Madalina Irimia <irimiamadamaria@gmail.com>
2026-01-12 16:30:23 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Irimia_Madalina_Lab1_BAMI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
