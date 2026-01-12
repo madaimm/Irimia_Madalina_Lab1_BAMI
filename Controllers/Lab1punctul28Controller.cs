f3071f649d145bc573ded120226e3a85f9f12357
Madalina Irimia <irimiamadamaria@gmail.com>
2026-01-12 16:53:51 +0200

    Am adaugat controller-ul Lab1punctul28Controller. Am adaugat un string Index de Bun venit poate fi vazut prin url: https://localhost:7019/lab1punctul28
    un string de salutare: https://localhost:7019/lab1punctul28/Salutare
    
    si un string personalizat utilizatorului cu nume si ziua cand a accesat: https://localhost:7019/lab1punctul28/InfoUtilizator?nume=Ema&ziua=12

---
+++ Controllers/Lab1punctul28Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Irimia_Madalina_Lab1_BAMI.Controllers
+{
+    public class Lab1punctul28Controller : Controller
+    {
+        public string Index()
+        {
+            return "Bine ai venit la primul meu laborator de ASP.NET Core MVC!";
+        }
+
+      
+        public string Salutare()
+        {
+            return "Salut! Sunt Madalina.";
+        }
+
+        public string InfoUtilizator(string nume)
+        {
+            
+            DateTime dataCurenta = DateTime.Now;
+
+            return "Salut " + nume + "! Ai accesat această pagină în data de: " + dataCurenta.ToString();
+        }
+    }
+}
